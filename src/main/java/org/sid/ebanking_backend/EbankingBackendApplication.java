14408f669659edb12595245c5d5513719f99b04e
Abdessamad-PRO <abdessamadaitelmahjoub2011@gmail.com>
2026-05-07 00:07:57 +0100

    Remplissage de la base de données avec les informations des clients

--- src/main/java/org/sid/ebanking_backend/EbankingBackendApplication.java
+++ src/main/java/org/sid/ebanking_backend/EbankingBackendApplication.java
@@ -1,13 +1,60 @@
 package org.sid.ebanking_backend;
 
+import org.sid.ebanking_backend.entities.CurrentAccount;
+import org.sid.ebanking_backend.entities.Customer;
+import org.sid.ebanking_backend.entities.SavingAccount;
+import org.sid.ebanking_backend.enums.AccountStatus;
+import org.sid.ebanking_backend.repositories.AccountOperationRepository;
+import org.sid.ebanking_backend.repositories.BankAccountRepository;
+import org.sid.ebanking_backend.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.awt.event.ActionListener;
+import java.util.Date;
+import java.util.UUID;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class EbankingBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EbankingBackendApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner start(CustomerRepository customerRepository,
+							BankAccountRepository bankAccountRepository,
+							AccountOperationRepository accountOperationRepository){
+		return args -> {
+			Stream.of("Hassan","Yassine","Aicha").forEach(name->{
+				Customer customer = new Customer();
+				customer.setName(name);
+				customer.setEmail(name+"@gmail.com");
+				customerRepository.save(customer);
+			});
+			customerRepository.findAll().forEach(cust->{
+				CurrentAccount currentAccount = new CurrentAccount();
+				currentAccount.setId(UUID.randomUUID().toString());
+				currentAccount.setBalance(Math.random()*9000);
+				currentAccount.setCreatedAt(new Date());
+				currentAccount.setStatus(AccountStatus.CREATED);
+				currentAccount.setCustomer(cust);
+				currentAccount.setOverDraft(9000);
+				bankAccountRepository.save(currentAccount);
+
+				SavingAccount savingAccount = new SavingAccount();
+				savingAccount.setId(UUID.randomUUID().toString());
+				savingAccount.setBalance(Math.random()*90000);
+				savingAccount.setCreatedAt(new Date());
+				savingAccount.setStatus(AccountStatus.CREATED);
+				savingAccount.setCustomer(cust);
+				savingAccount.setInterestRate(5.5);
+				bankAccountRepository.save(savingAccount);
+			});
+
+		};
 	}
 
 }
--- src/main/java/org/sid/ebanking_backend/entities/AccountOperation.java
+++ src/main/java/org/sid/ebanking_backend/entities/AccountOperation.java
@@ -13,6 +13,7 @@ public class AccountOperation {
     private Long id;
     private Date operationDate;
     private double amount;
+    @Enumerated(EnumType.STRING)
     private OperationType type;
     @ManyToOne
     private BankAccount bankAccount;
