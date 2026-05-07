4bdd8e392f877ffec296c0650f1adbf7a4ccbf62
Abdessamad-PRO <abdessamadaitelmahjoub2011@gmail.com>
2026-05-07 16:31:06 +0100

    Umplementation de la logique dans le service de banckAccountServiceImplementation avec gestion des exceptions par création de nouvelles exceptions

--- src/main/java/org/sid/ebanking_backend/services/BankAccountService.java
+++ src/main/java/org/sid/ebanking_backend/services/BankAccountService.java
@@ -1,17 +1,28 @@
 package org.sid.ebanking_backend.services;
 
 import org.sid.ebanking_backend.entities.BankAccount;
+import org.sid.ebanking_backend.entities.CurrentAccount;
 import org.sid.ebanking_backend.entities.Customer;
+import org.sid.ebanking_backend.entities.SavingAccount;
 import org.sid.ebanking_backend.repositories.BankAccountRepository;
 
 import java.util.List;
 
 public interface BankAccountService {
     Customer saveCustomer(Customer customer);
-    BankAccount saveBankAccount(double intialBalance, String type , Long customerId );
+
+    CurrentAccount saveCurrentBankAccount(double intialBalance, double overDraft, Long customerId );
+
+    SavingAccount saveSavingBankAccount(double intialBalance, double interestRate, Long customerId );
+
     List<Customer> listCustomer();
+
     BankAccount getBankAccount(String accountId);
+
     void debit(String accountId,double amount, String description);
+
     void credit(String accountId,double amount, String description);
+
     void transfer(String accountIdSource, String AccountIdDestination, double amount);
+
 }
--- src/main/java/org/sid/ebanking_backend/services/BankAccountServiceImpl.java
+++ src/main/java/org/sid/ebanking_backend/services/BankAccountServiceImpl.java
@@ -1,4 +1,100 @@
 package org.sid.ebanking_backend.services;
 
-public class BankAccountServiceImpl {
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.sid.ebanking_backend.entities.BankAccount;
+import org.sid.ebanking_backend.entities.CurrentAccount;
+import org.sid.ebanking_backend.entities.Customer;
+import org.sid.ebanking_backend.entities.SavingAccount;
+import org.sid.ebanking_backend.exceptions.BankAccountNotFoundException;
+import org.sid.ebanking_backend.exceptions.CustomerNotFoundException;
+import org.sid.ebanking_backend.repositories.AccountOperationRepository;
+import org.sid.ebanking_backend.repositories.BankAccountRepository;
+import org.sid.ebanking_backend.repositories.CustomerRepository;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+@Service
+@Transactional
+@AllArgsConstructor
+@Slf4j
+
+public class BankAccountServiceImpl implements BankAccountService {
+    private CustomerRepository customerRepository;
+    private BankAccountRepository bankAccountRepository;
+    private AccountOperationRepository accountOperationRepository;
+
+    @Override
+    public Customer saveCustomer(Customer customer) {
+        Customer savedCustomer = customerRepository.save(customer);
+        return savedCustomer;
+    }
+
+    @Override
+    public CurrentAccount saveCurrentBankAccount(double intialBalance, double overDraft, Long customerId) throws CustomerNotFoundException {
+        Customer customer = customerRepository.findById(customerId).orElse(null);
+        if(customer==null){
+            throw new CustomerNotFoundException("Customer not found");
+        }
+        CurrentAccount currentAccount = new CurrentAccount();
+
+        currentAccount.setId(UUID.randomUUID().toString());
+        currentAccount.setCreatedAt(new Date());
+        currentAccount.setBalance(intialBalance);
+        currentAccount.setCustomer(customer);
+        currentAccount.setOverDraft(overDraft);
+        CurrentAccount savedBankAccount = bankAccountRepository.save(currentAccount);
+        return savedBankAccount;
+    }
+
+    @Override
+    public SavingAccount saveSavingBankAccount(double intialBalance, double interestRate, Long customerId) throws CustomerNotFoundException {
+        Customer customer = customerRepository.findById(customerId).orElse(null);
+        if(customer==null){
+            throw new CustomerNotFoundException("Customer not found");
+        }
+        SavingAccount savingAccount = new SavingAccount();
+        savingAccount.setId(UUID.randomUUID().toString());
+        savingAccount.setCreatedAt(new Date());
+        savingAccount.setBalance(intialBalance);
+        savingAccount.setCustomer(customer);
+        savingAccount.setInterestRate(interestRate);
+        SavingAccount savedBankAccount = bankAccountRepository.save(savingAccount);
+        return savedBankAccount;
+
+    }
+
+
+    @Override
+    public List<Customer> listCustomer() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public BankAccount getBankAccount(String accountId) throws BankAccountNotFoundException {
+        BankAccount bankAccount = bankAccountRepository.findById(accountId)
+                .orElseThrow(()->new BankAccountNotFoundException("Bankccount not found"));
+        return bankAccount;
+    }
+
+    @Override
+    public void debit(String accountId, double amount, String description) {
+
+    }
+
+    @Override
+    public void credit(String accountId, double amount, String description) {
+
+    }
+
+    @Override
+    public void transfer(String accountIdSource, String AccountIdDestination, double amount) {
+
+    }
 }
