7d6f7dce5613e8ff593a9e348121b224f1bb4eb8
Abdessamad-PRO <abdessamadaitelmahjoub2011@gmail.com>
2026-05-07 19:29:11 +0100

    Création de controller pour customer et affichage de clients avec l'annotation JsonProperty et write_only

---
+++ src/main/java/org/sid/ebanking_backend/web/CustomerRestController.java
@@ -0,0 +1,22 @@
+package org.sid.ebanking_backend.web;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.sid.ebanking_backend.entities.Customer;
+import org.sid.ebanking_backend.services.BankAccountService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+public class CustomerRestController {
+    private BankAccountService bankAccountService;
+
+    @GetMapping("/customers")
+    public List<Customer> customers(){
+        return bankAccountService.listCustomer();
+    }
+}
