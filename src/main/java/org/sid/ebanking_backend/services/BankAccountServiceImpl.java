e2f9e9be27bc23d13fdad5bf535231c80b2c6154
Abdessamad-PRO <abdessamadaitelmahjoub2011@gmail.com>
2026-07-14 18:16:08 +0100

    Gestion de la sécurité stateful de l'application avec inmemory saver

--- src/main/java/org/sid/ebanking_backend/services/BankAccountServiceImpl.java
+++ src/main/java/org/sid/ebanking_backend/services/BankAccountServiceImpl.java
@@ -265,7 +265,7 @@ public class BankAccountServiceImpl implements BankAccountService {
      @Override
     public AccountHistoryDTO getAccountHistory(String accountId, int page , int size) throws BankAccountNotFoundException{
         BankAccount bankAccount = bankAccountRepository.findById(accountId).orElse(null);
-        Page<AccountOperation> accountOperations = accountOperationRepository.findByBankAccountId(accountId, PageRequest.of(page,size));
+        Page<AccountOperation> accountOperations = accountOperationRepository.findByBankAccountIdOrderByOperationDateDesc(accountId, PageRequest.of(page,size)); //********** ici**********
         AccountHistoryDTO  accountHistoryDTO= new AccountHistoryDTO();
         List<AccountOperationDTO> accountOperationDTOS = accountOperations.getContent().stream().map(op->dtoMapper.fromAccountOperation(op)).collect(Collectors.toList());
         accountHistoryDTO.setAccountOperationDTOS(accountOperationDTOS);
